5ae5454544aa88971b4be70a4ebce604fd58d90e
sachetsharma <sachet.us@gmail.com>
2014-10-14 14:30:46 -0400

    some changes

--- app.js
+++ app.js
@@ -4,11 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var monk = require('monk');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
+
 var app = express();
+
+mongoose.connect('mongodb://localhost:27017/meandb');
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+    console.log('error', err);
+});
+
+db.on('open', function () {
+    console.log('connection open');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +38,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+    req.db = db;
+    next();
+});
+
 app.use('/', routes);
 app.use('/users', users);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,36 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/helloworld', function (req, res) {
+    res.render('helloworld', { title: "Hello, World!" });
+});
+
+router.get('/userlist', function (req, res) {
+    var db = req.db;
+    var collection = db.collection('userCollection');
+    collection.find({}).toArray(function (e,doc)
+    {
+        res.render('userlist', { users: doc });
+    });
+});
+
+router.get('/newuser', function (req, res) {
+    res.render('newuser', { title: 'New User'});
+});
+
+router.post('/adduser', function (req, res) {
+    var db = req.db;
+    var username = req.body.username;
+    var useremail = req.body.useremail;
+    var collection = db.collection('userCollection');
+    collection.insert({ username: username, email: useremail }, function (e, doc) {
+        if (e) {
+            res.send("there was a problem adding the user.");
+        }
+        else {
+            res.location("userlist");
+            res.redirect("userlist");
+        }
+    });
+});
 module.exports = router;
