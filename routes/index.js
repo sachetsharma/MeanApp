b711e08851b43b6840dc2b72e55b199226fdb6a6
sachetsharma <sachet.us@gmail.com>
2014-10-17 20:42:56 -0400

    some changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('home', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 router.get('/helloworld', function (req, res) {
